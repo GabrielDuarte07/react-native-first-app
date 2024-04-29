d0d911cb1a807c16e2d67533e4bad01e2f52a868
Gabriel Duarte <gabriel.gd7@outlook.com>
2024-04-29 10:48:56 -0300

    text input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Platform,
+} from 'react-native';
 
 const Home = (): React.JSX.Element => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello World!</Text>
+        <TextInput style={styles.textInput} />
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#121214',
+  },
   container: {
     display: 'flex',
     height: '100%',
     width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: '#121214',
+    paddingHorizontal: 30,
+    paddingVertical: 50,
   },
   title: {
     color: '#f1f1f1',
+    fontSize: 24,
+  },
+  textInput: {
+    fontSize: 18,
+    backgroundColor: '#29292e',
+    color: '#f1f1f1',
+    marginTop: 30,
+    borderRadius: 7,
+    padding: Platform.OS === 'ios' ? 15 : 12,
   },
 });
 
